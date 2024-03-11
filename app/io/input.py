2a7f20215aafc4f7759f5095b4977ac6dddd6cbc
anastasiia1210 <anastasiia.lysa@ukma.edu.ua>
2024-03-11 20:53:12 +0200

    create functions for input and output data

---
+++ app/io/input.py
@@ -0,0 +1,11 @@
+def input_text():
+    pass
+
+def read_file():
+    pass
+
+def read_file_pandas():
+    pass
+
+if __name__ == "__main__":
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,8 @@
+def output_text(text):
+    pass
+
+def write_file(data, file):
+    pass
+
+if __name__ == "__main__":
+    pass
