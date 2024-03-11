d354e6f219eda73768b70df170bfa18314a98e58
anastasiia1210 <anastasiia.lysa@ukma.edu.ua>
2024-03-11 22:03:41 +0200

    realization of functions

--- app/io/input.py
+++ app/io/input.py
@@ -1,11 +1,38 @@
+import pandas as pd
+
 def input_text():
-    pass
+    """
+    Function to input text from the console.
+    Returns:
+        str: The text inputted from the console.
+    """
+    return input("Enter text: ")
 
 def read_file():
-    pass
+    """
+    Function to read from a file.
+    Returns:
+        str: The content read from the file.
+    """
+    try:
+        with open("input_file.txt", "r") as file:
+            return file.read()
+    except FileNotFoundError:
+        print("File not found.")
+        return ""
 
 def read_file_pandas():
-    pass
+    """
+    Function to read from a file using Pandas.
+    Returns:
+        pandas.DataFrame: The data read from the file as a DataFrame, or None if file not found.
+    """
+    try:
+        df = pd.read_csv("input_file.txt")
+        return df
+    except FileNotFoundError:
+        print("File not found pandas.")
+        return None
 
 if __name__ == "__main__":
     pass
--- app/io/output.py
+++ app/io/output.py
@@ -1,8 +1,24 @@
 def output_text(text):
-    pass
+    """
+    Function to print text to the console.
+    Args:
+        text (str): The text to be printed to the console.
+    """
+    print(text)
 
 def write_file(data, file):
-    pass
+    """
+    Function to write data to a file.
+    Args:
+        data: The data to be written to the file.
+        file (str): The name of the file to write to.
+    """
+    try:
+        with open(file, "w") as f:
+            f.write(data)
+        print(f"Data written to file '{file}' successfully.")
+    except Exception as e:
+        print(f"Error writing data to file: {e}")
 
 if __name__ == "__main__":
     pass
